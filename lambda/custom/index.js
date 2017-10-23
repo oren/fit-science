e64ac146e9591bc14d6d40103ceb9850e7d49aa8
Oren <orengolan@gmail.com>
2017-10-23 09:06:32 -0700

    misc

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -15,12 +15,15 @@ var handlers = {
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
+
     'HelloWorldIntent': function () {
         this.emit('SayHello');
     },
+
     'MyNameIsIntent': function () {
         this.emit('SayHelloName');
     },
+
     'SayHello': function () {
         var say = 'Fit Science help you lose fat or get stronger based on your goals and preferences. Do you want to lose fat, get stronger, or both? You can also ask me for list of programs.'
         this.response.speak(say)
@@ -28,28 +31,34 @@ var handlers = {
 
         this.emit(':responseReady');
     },
+
     'SayHelloName': function () {
         var name = this.event.request.intent.slots.name.value;
         this.response.speak('Hello ' + name)
             .cardRenderer('hello world', 'hello ' + name);
         this.emit(':responseReady');
     },
+
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
     },
+
     'AMAZON.StopIntent' : function() {
         this.response.speak('Bye');
         this.emit(':responseReady');
     },
+
     'AMAZON.HelpIntent' : function() {
         this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
             " name is awesome Aaron'");
         this.emit(':responseReady');
     },
+
     'AMAZON.CancelIntent' : function() {
         this.response.speak('Bye');
         this.emit(':responseReady');
     },
+
     'Unhandled' : function() {
         this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
             " or 'alexa, ask hello world my name is awesome Aaron'");
